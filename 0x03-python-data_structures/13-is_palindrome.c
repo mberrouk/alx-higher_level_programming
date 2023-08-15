d8c60aa23b67a2a3b8dbbcd771d58241a1f1c4a4
mberrouk <mberrouk@student.1337.ma>
2023-08-14 18:43:29 -0700

    13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,85 @@
+#include "lists.h"
+
+/**
+ * lst_size - count nodes in a linked list
+ * @head: pointer to the head node
+ *
+ * Return: length of linked list
+ */
+int lst_size(listint_t *head)
+{
+	int len;
+
+	len = 0;
+	while (head)
+	{
+		len++;
+		head = head->next;
+	}
+	return (len);
+}
+
+/**
+ * revers_list - reverse a linked list
+ * @head: pointer to head node
+ *
+ * Return: a pointer to a head
+ */
+listint_t *revers_list(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *next;
+	listint_t *prev;
+
+	tmp = *head;
+	while (tmp)
+	{
+		next = tmp->next;
+		tmp->next = prev;
+		prev = tmp;
+		tmp = next;
+	}
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0 else 0.
+ */
+int is_palindrome(listint_t **head)
+{
+	int size;
+	int i;
+	listint_t *tmp;
+	listint_t *mid;
+	listint_t *h;
+
+
+	if (!head || !*head || !(*head)->next)
+		return (0);
+	tmp = *head;
+	size = lst_size(tmp);
+	i = 0;
+	while (i < (size / 2) - 1)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	if (!(size % 2) && tmp->n != tmp->next->n)
+		return (0);
+	mid = revers_list(&tmp->next);
+	tmp = *head;
+	h = mid;
+	while (h)
+	{
+		if (h->n != tmp->n)
+			return (0);
+		h = h->next;
+		tmp = tmp->next;
+	}
+	return (1);
+
+}
