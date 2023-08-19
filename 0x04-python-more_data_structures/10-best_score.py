0e5625cbf7ba3c17e6476f1e201ca0693f327b6f
mberrouk <mberrouk@student.1337.ma>
2023-08-18 17:07:54 -0700

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+
+
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    mk = list(a_dictionary.keys())[0]
+    mx = a_dictionary[mk]
+    for k, v in a_dictionary.items():
+        if mx < v:
+            mx = v
+            mk = k
+    return (mk)
