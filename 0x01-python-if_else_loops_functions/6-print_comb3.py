878309c2c884e91fdb7abf082ab19d621047f47c
Mohamed Berroukham <mberrouk@c3r9p2.1337.ma>
2023-08-27 16:31:52 +0100

    add a newline

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,6 +5,6 @@ i = 0
 while (i < 9):
     j = i + 1
     while (j <= 9):
-        print("{}{}".format(i, j), end=", ")
+        print("{}{}".format(i, j), end=", " if i < 8 else "\n")
         j += 1
     i += 1
