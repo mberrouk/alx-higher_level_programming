ec6ee77c5ade443c08afe2e959a107ad36402234
mberrouk <mberrouk@student.1337.ma>
2023-08-28 10:10:46 -0700

    fix lenght

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     n = 0
     for i in range(x):
         try:
-            n += 1
             print("{}".format(my_list[i]), end="")
+            n += 1
         except:
             break
     print("")
