05dab1f768b7c07ad0b9d9d70bb598acf4050787
Mohamed Berroukham <mberrouk@c3r10p9.1337.ma>
2023-08-28 17:53:52 +0100

    corect return

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,7 @@ def safe_print_list(my_list=[], x=0):
     n = 0
     for i in range(x):
         try:
+            n += 1
             print("{}".format(my_list[i]), end="")
         except IndexError:
             break
