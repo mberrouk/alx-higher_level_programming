dfc39a71286a201bd001f82fff05a0096411cc46
mberrouk <mberrouk@student.1337.ma>
2023-08-28 10:19:39 -0700

    fix all

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             n += 1
-        except:
+        except Exception:
             break
     print("")
     return (n)
