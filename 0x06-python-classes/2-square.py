91c7684b334d8e7227ebe80a121582427fcd3414
mberrouk <mberrouk@student.1337.ma>
2023-08-30 16:10:18 -0700

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write Square"""
+
+
+class Square:
+    """ Square class defines a square """
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
