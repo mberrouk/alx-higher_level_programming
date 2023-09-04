7d64cc5b37e6c13f45f8addcc552111b52b27eb9
mberrouk <mberrouk@student.1337.ma>
2023-09-04 16:42:32 -0700

    &

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
             TypeError: value is not an integer
             ValueError: value is less than zero
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
