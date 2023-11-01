66c9b00e9be290d71528851f4c11d1ce0b2a752c
mberrouk <mberrouk@student.1337.ma>
2023-11-01 16:31:23 -0700

    101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+Module: The N queens puzzle is the challenge of placing N non-attacking queens
+on an N×N chessboard. Write a program that solves the N queens problem.
 
+    Usage: nqueens N
+        - If the user called the program with the wrong number of arguments,
+        print Usage: nqueens N, followed by a new line, and exit with the
+        status 1
+    Where N must be an integer greater or equal to 4:
+        - If N is not an integer, print N must be a number, followed by a
+        new line, and exit with the status 1
+        - If N is smaller than 4, print N must be at least 4, followed by
+        a new line, and exit with the status 1
+    The program should print every possible solution to the problem:
+        - One solution per line
+"""
 
 
 def check(aqueen, n_queen):
@@ -33,11 +48,15 @@ def Queen(aqueen, n_queen):
             if n_queen < len(aqueen):
                 Queen(aqueen, n_queen + 1)
 
+
 def init_backtracker(n):
+    """ initial algorithem"""
     queen = [-1 for x in range(n)]
     Queen(queen, 0)
 
+
 if __name__ == "__main__":
+    """ first step """
     import sys
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
