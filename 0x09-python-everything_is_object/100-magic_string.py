7e7baea71ec6f1a0b41aa36f16d4ae3cd7e5c10e
mberrouk <mberrouk@student.1337.ma>
2023-11-01 14:30:17 -0700

    100

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module: function return a string "BestSchool".
+"""
+
+
+def magic_string():
+    """
+    function that returns a string n times.
+    """
+    magic_string.n = getattr(magic_string, "n", 0) + 1
+    return (("BestSchool, " * (magic_string.n - 1)) + "BestSchool")
