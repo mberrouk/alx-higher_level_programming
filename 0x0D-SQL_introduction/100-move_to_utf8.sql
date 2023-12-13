f6cd30e2564265ab8460daf0186caec9d2e6c527
mberrouk <mberrouk@student.1337.ma>
2023-12-12 17:33:48 -0800

    100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,6 +2,6 @@
 -- For each database:
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- For each table:
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER COLLATE utf8mb4_unicode_ci;
 -- For each column:
-ALTER TABLE hbtn_0c_0.first_table  CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CHANGE name name VARCHAR(256) CHARACTER COLLATE utf8mb4_unicode_ci;
