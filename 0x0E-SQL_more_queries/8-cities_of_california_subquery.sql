7c53a0223228cd358e6f9bcd367db65d8c34353b
mberrouk <mberrouk@student.1337.ma>
2023-12-14 01:30:45 +0100

    init

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,2 @@
 -- lists all the cities of California.
-SELECT * FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id;
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id;
