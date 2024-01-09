4e8eb773b20bff8be8e6cd7ab26304bb8f05d89d
mberrouk <mberrouk@student.1337.ma>
2024-01-09 01:46:49 +0000

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const n = parseInt(process.argv[2]);
 if (isNaN(n)) {
   console.log('Missing size');
 } else {
-	let output = '';
-	for (let i = 0; i < n; i++) output += 'X'
+  let output = '';
+  for (let i = 0; i < n; i++) output += 'X';
   for (let j = 0; j < n; j++) console.log(output);
 }
