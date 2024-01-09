90c91060e647fe9b4a7c021e3c14dca5d2fd311e
mberrouk <mberrouk@student.1337.ma>
2024-01-09 03:21:35 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const arg = process.argv;
-let res = NaN;
-let tmp;
-
-arg.forEach((val, index) => {
-  if (!isNaN(tmp = parseInt(val)) && (isNaN(res) || res < tmp)) {
-    res = tmp;
-  }
-});
-
+let res;
+let args;
+if (process.argv.length < 4) {
+  res = 0;
+} else {
+  args = process.argv.slice(2).map(Number);
+  args.sort((a, b) => b - a);
+  res = args[1];
+}
 console.log(res);
